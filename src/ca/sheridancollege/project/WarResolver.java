13ca48aa6123706ce484d214f78321986e360f55
Juan Munoz <munoj@sheridancollege.ca>
2026-04-06 20:17:10 -0400

    this is branch version 3 Signed-off-by:Juan Munoz <munoj@sheridancollege.ca>

--- src/ca/sheridancollege/project/WarResolver.java
+++ src/ca/sheridancollege/project/WarResolver.java
@@ -17,36 +17,49 @@ public class WarResolver {
         this.comparator = comparator;
     }
 
+    
     public Player resolveWar(Player user, Player computer) {
 
         ui.showTieStart();
 
-        if (!user.hasCards() || !computer.hasCards()) {
-            ui.showRoundWinner("A player ran out of cards during WAR!");
+        // Need 3 cards each: 2 face-down + 1 face-up
+        if (!user.hasAtLeast(3) || !computer.hasAtLeast(3)) {
+            ui.showRoundWinner("A player does not have enough cards to continue WAR (needs 3).");
             return null;
         }
 
-        Card userTieCard = user.playCard();
-        Card computerTieCard = computer.playCard();
+        // 2 cards face-down (burn cards)
+        Card userDown1 = user.playCard();
+        Card userDown2 = user.playCard();
+        Card compDown1 = computer.playCard();
+        Card compDown2 = computer.playCard();
 
-        if (userTieCard == null || computerTieCard == null) {
+        // 3rd card face-up
+        Card userUp = user.playCard();
+        Card compUp = computer.playCard();
+
+        // Defensive (shouldn't happen because hasAtLeast(3), but safe)
+        if (userUp == null || compUp == null) {
             ui.showRoundWinner("Unable to continue WAR due to lack of cards.");
             return null;
         }
 
-        ui.showTieCards(userTieCard, computerTieCard);
+        // Optional UI messaging
+        ui.showRoundWinner("Each player places 2 cards face-down...");
+        ui.showTieCards(userUp, compUp); // reuse your existing UI method for the face-up cards
 
-        int result = comparator.compare(userTieCard, computerTieCard);
+        int result = comparator.compare(userUp, compUp);
 
         if (result > 0) {
-            ui.showRoundWinner("You win the tie-breaker round!");
+            ui.showRoundWinner("You win the WAR!");
             return user;
         } else if (result < 0) {
-            ui.showRoundWinner("Computer wins the tie-breaker round!");
+            ui.showRoundWinner("Computer wins the WAR!");
             return computer;
         } else {
-            ui.showRoundWinner("It's another tie! Continuing WAR...");
-            return resolveWar(user, computer);
+            ui.showRoundWinner("WAR tied again! Continuing...");
+            return resolveWar(user, computer); // repeat 2-down + 1-up
         }
     }
+
 }
